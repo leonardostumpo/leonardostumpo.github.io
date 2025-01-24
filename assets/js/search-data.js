c8f6371d4683e3e55111e7867d2f9554cc710aee
leonardostumpo <95302999+leonardostumpo@users.noreply.github.com>
2025-01-24 17:09:39 +0000

    Deploying to gh-pages from @ leonardostumpo/leonardostumpo.github.io@9336ec5467407db4b954dffc227b587115e71a94 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,14 +492,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6C%65%6F%6E%61%72%64%6F.%73%74%75%6D%70%6F@%70%6F%6C%69%74%6F.%69%74", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/leonardostumpo", "_blank");
         },
       },{
         id: 'social-rss',
@@ -513,14 +513,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=rVG6_aUAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
